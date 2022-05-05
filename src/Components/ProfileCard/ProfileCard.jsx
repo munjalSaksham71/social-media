185df0fe098d0f77db79fe2adea6d2780208e9b1
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-05-05 22:37:18 +0530

    Feat: Show Follwer and following on profile card

--- src/Components/ProfileCard/ProfileCard.jsx
+++ src/Components/ProfileCard/ProfileCard.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
+import { getUserById } from "../../actions/userAction";
 import { useFollower } from "../../context/follower-context";
 import Card from "../Card/Card";
 import "./ProfileCard.css";
 
 const ProfileCard = () => {
   const {
-    followState: { following }
+    followState: { following, followers }, 
+    followDispatch
   } = useFollower();
+  
+  useEffect(() => {
+    (async () => {
+      const data = await getUserById(userData._id)
+      followDispatch({type: 'GET_USER_FOLLOWING', payload: data.user.following})
+      followDispatch({type: 'GET_USER_FOLLOWERS', payload: data.user.followers})
+    })()
+  }, [])
 
   const userData = JSON.parse(localStorage.getItem("userData"))
 
@@ -20,12 +31,15 @@ const ProfileCard = () => {
         <div className="flex-column profile_info">
           <div className="profile_name">{`${userData.firstName} ${userData.lastName} `}</div>
           <div className="profile_username">{userData.username}</div>
-          <div className="flex-row profile-summary">
+        </div>
+        <div className="flex-row profile-summary">
+          <div className="profile-following">
+              {followers.length} Followers
+          </div>
             <div className="profile-following">
               {following.length} Following
             </div>
           </div>
-        </div>
       </div>
     </Card>
   );
--- src/reducer/followReducer.js
+++ src/reducer/followReducer.js
@@ -1,12 +1,16 @@
 export const followerInitialState = {
-  following: []
+  following: [], 
+  followers: []
 };
 
 export const followReducer = (state, action) => {
   switch (action.type) {
     case "FOLLOW":
     case "UNFOLLOW":
+    case "GET_USER_FOLLOWING":
       return { ...state, following: action.payload };
+    case "GET_USER_FOLLOWERS":
+      return {...state, followers: action.payload}
     case "CLEAR_DATA": 
     return {...state, following: []}
     default:
